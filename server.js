f77d780e20cd30d6e2d04bfd64cce89bffb1990c
Herman Korolevsky <h.korolevsky@gmail.com>
2018-03-28 23:33:05 +0300

    added missing files

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
 // your manifest must have appropriate CORS headers, you could also use '*'
-app.use(cors({ origin: '*' }));
+app.use(cors({origin: '*'}));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("*", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + '/views/index.html');
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
 });
